0b813650193b3efdacd8d64ae379a1c313073f34
spencercreer <spencercreer@gmail.com>
2021-03-17 22:47:14 -0600

    setup schema.sql and seeds.sql files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+/* Create and use the burgers_db db */
+DROP DATABASE IF EXISTS `burgers_db`;
+CREATE DATABASE `burgers_db`;
+USE `burgers_db`;
+
+
+/* Create a table for all your star wars characters */
+CREATE TABLE `burgers` (
+	`id` Int( 20 ) AUTO_INCREMENT NOT NULL,
+	`burger_name` VARCHAR( 255) NOT NULL,
+	`devoured` BOOLEAN NOT NULL,
+	/* Set ID as primary key */
+	PRIMARY KEY ( `id` )
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
